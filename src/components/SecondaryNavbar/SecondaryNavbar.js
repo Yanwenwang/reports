11a467ccd91961c9db15dc902a1f6e33adb16d6d
Yanwen <ywwenwang@gmail.com>
2018-06-10 17:39:59 -0700

    Add secondary navbar.

---
+++ src/components/SecondaryNavbar/SecondaryNavbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SecondaryNavbar.scss';
+
+export const SecondaryNavbar = () => {
+    return (
+        <div className="secondarynav__container"></div>
+    );
+};
+
+export default SecondaryNavbar;
--- src/layouts/Corelayout/Corelayout.js
+++ src/layouts/Corelayout/Corelayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import SecondaryNavbar from '../../components/SecondaryNavbar/SecondaryNavbar';
 import Home from '../../components/Home/Home';
 import '../../styles/core.scss';
 
@@ -7,6 +8,7 @@ export const Corelayout = ({ children }) => {
     return (
         <React.Fragment>
             <Navbar />
+            <SecondaryNavbar />
             <Home />
         </React.Fragment>
     );
