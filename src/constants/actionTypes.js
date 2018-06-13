251eb929cc73adc95bbac02fdf5268cc43eeb5dd
Yanwen <ywwenwang@gmail.com>
2018-06-12 22:47:45 -0700

    Add candidates action types.

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,14 @@
+const actionTypes = {
+    CANDIDATES: {
+        GET: {
+            CALL: 'CANDIDATES.GET.CALL',
+            SUCCESS: 'CANDIDATES.GET.SUCCESS',
+            FAIL: 'CANDIDATES.GET.FAIL'
+        }
+    },
+    ROUTER: {
+        LOCATION_CHANGE: '@@router/LOCATION_CHANGE'
+    }
+};
+
+export default actionTypes;
